b0fdfd686df87ab8f29631c20b3e0114ffddc9b0
JOOZOO20 <qwsa7896@naver.com>
2020-12-28 12:39:38 +0900

    자바 RPG게임 최종 직전 - Character클래스
    사용자 캐릭터의 정보를 담고 있는 Character 클래스

---
+++ re2/Character.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Character {
+    static int money;
+    static String name;
+    static String Weapon;
+    static int[] level = {1, 2, 3};
+    static int healthPower = 100;
+    static int attackPower = 10;
+    static int defencePower = 3;
+
+//    public void showCharacterInformation(){
+//        Information show = new Information();
+//        show.showPlayerInformation_level(show);
+//    }
+
+}
+
