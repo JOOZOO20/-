89358ca6da0c728cf074303677ed464448f41075
JOOZOO20 <qwsa7896@naver.com>
2020-12-28 12:41:36 +0900

    자바 RPG게임 최종직전 - Level 클래스
    기본 캐릭터의 level의 정보를 담고 있는 클래스

---
+++ re2/Level.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Level {
+    int healthPower;
+    int attackPower;
+    int defencePower;
+    Level (int healthPower, int attackPower, int defencePower) {
+        this.healthPower = healthPower;
+        this.attackPower = attackPower;
+        this.defencePower= defencePower;
+    }
+}
+
