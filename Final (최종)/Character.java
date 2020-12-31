dc6c9c1c6f58933855e7654f646d7269034866d9
JOOZOO20 <qwsa7896@naver.com>
2020-12-31 12:37:56 +0900

    자바 RPG게임 최종 직전 - Character클래스
    사용자 캐릭터의 정보를 담고 있는 Character 클래스

---
+++ Final (최종)/Character.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Character {
+    static int money;
+    static String name;
+    static String Weapon;
+    static int level = 1;
+    static int healthPower = 100;
+    static int attackPower = 10;
+    static int defencePower = 3;
+
+}
+
