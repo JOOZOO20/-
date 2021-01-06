a33064cd33a250f631f71888d5d249e6d8eac5da
JOOZOO20 <qwsa7896@naver.com>
2021-01-06 18:00:21 +0900

    자바 RPG게임 최종 - Information클래스
    캐릭터의 정보를 보기 쉽게 표기하고 있는 information클래스

---
+++ Final(최종)/Information.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Information {
+    Character character = new Character();
+    Lobby reGo;
+
+    Information(){
+        this.reGo = new Lobby();
+    }
+
+
+    public void showPlayerInformation_level(Information input) {
+        System.out.println("이름 : " + Character.name);
+        System.out.println("레벨 : " + Character.level);
+        System.out.println("체력 : " + Character.healthPower);
+        System.out.println("공격력 : " + Character.attackPower);
+        System.out.println("방어력 : " + Character.defencePower);
+        System.out.println("가진 돈 : " + Character.money);
+        System.out.println("------------------------------------");
+        System.out.println("로비로 이동하겠습니다.");
+        try {
+            Thread.sleep(1000);
+            reGo.goWherePlace();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+}
